b79c1a5f720ad39b3f118c6d4ab6cf34cfd974f7
ojpro <contact@ojpro.me>
2022-06-27 12:36:16 +0100

    test owner.

--- server/tests/Feature/Models/OwnerTest.php
+++ server/tests/Feature/Models/OwnerTest.php
@@ -2,6 +2,8 @@
 
 namespace Tests\Feature\Models;
 
+use App\Models\Gym;
+use App\Models\Owner;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -13,10 +15,38 @@ class OwnerTest extends TestCase
      *
      * @return void
      */
-    public function test_example()
+    public function example()
     {
         $response = $this->get('/');
 
         $response->assertStatus(200);
+    }
+
+    /**
+     * Create new Owner
+     */
+    public function test_create_owner()
+    {
+        $owner = Owner::factory()->create();
+
+        $this->assertDatabaseCount('owners', 1);
+    }
+
+    /**
+     * List Owned GYMs
+     */
+    public function test_list_owned_gyms()
+    {
+        $owner = Owner::factory()->create();
+
+        Gym::factory()->count(3)->create([
+            'owner_id' => $owner['id']
+        ]);
+
+        $owned = Owner::findOrFail($owner['id'])->with('gyms')->first();
+
+        // TODO: improve assertion
+        $this->assertTrue(count($owned['gyms']->toArray()) === 3);
+
     }
 }
